8c506f32f091cfdbce1bd7c5abb8c3238ac92ec8
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-25 15:54:36 -0400

    Image on art page

--- src/pages/ArtPage.tsx
+++ src/pages/ArtPage.tsx
@@ -3,22 +3,20 @@ import Grid from "@material-ui/core/Grid";
 import ava from "../img/ava_square.png";
 
 export const ArtPage: React.FC = () => {
-  // return (
-  //   <div>
-  //     <Grid>
-  //       <Grid item xs>
-  //         <ImageBlock src={ava} />
-  //       </Grid>
-  //     </Grid>
-  //   </div>
-  // );
-
-  return <div>art page</div>;
+  return (
+    <div>
+      <Grid>
+        <Grid item xs>
+          <ImageBlock src={ava} />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 interface ImageBlockProps {
   src: string;
 }
 const ImageBlock: React.FC<ImageBlockProps> = props => {
-  return <img src={props.src} />;
+  return <img style={{ width: "50%", boxShadow: "2px" }} src={props.src} />;
 };
