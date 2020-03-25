aae2ccf745aaf261a4040994c11692ab804e70ef
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-25 14:33:48 -0400

    Move images to src

--- src/pages/ArtPage.tsx
+++ src/pages/ArtPage.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import ava from "../img/ava_square.png";
 
 export const ArtPage: React.FC = () => {
+  // return (
+  //   <div>
+  //     <Grid>
+  //       <Grid item xs>
+  //         <ImageBlock src={ava} />
+  //       </Grid>
+  //     </Grid>
+  //   </div>
+  // );
+
   return <div>art page</div>;
+};
+
+interface ImageBlockProps {
+  src: string;
+}
+const ImageBlock: React.FC<ImageBlockProps> = props => {
+  return <img src={props.src} />;
 };
