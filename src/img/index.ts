84283bdd00da065724c11a8bb3406b70e4e36e68
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-25 20:39:40 -0400

    Add all images

---
+++ src/img/index.ts
@@ -0,0 +1,39 @@
+import ava from "./ava_square.png";
+import chimpanzee from "./chimpanzee_square.jpeg";
+import cockatoo from "./cockatoo_square.jpeg";
+import crow from "./crow_square.jpeg";
+import femaleBody from "./female-body_square.jpeg";
+import femalePortrait from "./female-portrait_square.jpeg";
+import flower from "./flower_square.png";
+import frog from "./frog_square.jpeg";
+import hand from "./hand2_square.jpeg";
+import jellyfish from "./jellyfish_square.jpeg";
+import mask from "./mask_square.jpg";
+import mushroom from "./mushroom_square.jpg";
+import mushrooms from "./mushrooms_square.jpg";
+import orange from "./orange_square.jpg";
+import owl from "./owl_square.jpg";
+import peacock from "./peacock_square.jpg";
+import skull from "./skull_square.jpg";
+
+const images = [
+  ava,
+  chimpanzee,
+  cockatoo,
+  crow,
+  femaleBody,
+  femalePortrait,
+  flower,
+  frog,
+  hand,
+  jellyfish,
+  mask,
+  mushroom,
+  mushrooms,
+  orange,
+  owl,
+  peacock,
+  skull
+];
+
+export { images };
--- src/pages/ArtPage.tsx
+++ src/pages/ArtPage.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
-import ava from "../img/ava_square.png";
+import { images } from "../img";
 
 export const ArtPage: React.FC = () => {
   return (
     <div>
       <Grid>
-        <Grid item xs>
-          <ImageBlock src={ava} />
-        </Grid>
+        {images.map(i => (
+          <ImageBlock src={i} />
+        ))}
       </Grid>
     </div>
   );
 };
 
+const imageBlockStyle: React.CSSProperties = {
+  maxWidth: "400px",
+  display: "inline-block",
+  padding: "5px"
+};
+
 interface ImageBlockProps {
   src: string;
 }
+
 const ImageBlock: React.FC<ImageBlockProps> = props => {
-  return <img style={{ width: "50%", boxShadow: "2px" }} src={props.src} />;
+  return <img style={imageBlockStyle} src={props.src} />;
 };
