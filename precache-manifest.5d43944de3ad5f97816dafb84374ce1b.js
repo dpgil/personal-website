e88a0d1ba85118ebfdab696adc4dfbc3bbdf75e1
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-28 12:59:52 -0400

    Updates

---
+++ precache-manifest.5d43944de3ad5f97816dafb84374ce1b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305bba7d134296ff136d31386f68de37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d201418658fd61aa389",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9befded8782aa3ed8605",
+    "url": "/static/js/2.9c1beae9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9c1beae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d201418658fd61aa389",
+    "url": "/static/js/main.4c107f7c.chunk.js"
+  },
+  {
+    "revision": "0b4f3269348c2e5315e5",
+    "url": "/static/js/runtime-main.9089ee33.js"
+  },
+  {
+    "revision": "fb38a14f671de194c66fd7559606bb7f",
+    "url": "/static/media/ava_square.fb38a14f.png"
+  },
+  {
+    "revision": "d6c5a4418b00b4077cd81e2825e4b26d",
+    "url": "/static/media/chimpanzee_square.d6c5a441.jpeg"
+  },
+  {
+    "revision": "f918f5a180b14bd757822c1690695f3f",
+    "url": "/static/media/cockatoo_square.f918f5a1.jpeg"
+  },
+  {
+    "revision": "59589005be5415279cfb3720a798890c",
+    "url": "/static/media/crow_square.59589005.jpeg"
+  },
+  {
+    "revision": "a56a523272da17a6e690bff64de80715",
+    "url": "/static/media/female-body_square.a56a5232.jpeg"
+  },
+  {
+    "revision": "c4e8cbbbc85e2606ef649eef476a1bab",
+    "url": "/static/media/female-portrait_square.c4e8cbbb.jpeg"
+  },
+  {
+    "revision": "12bf63c50803bff429cbaa6542496abb",
+    "url": "/static/media/flower_square.12bf63c5.png"
+  },
+  {
+    "revision": "26d5a2ebe9595e5f9f934199bc8a16ef",
+    "url": "/static/media/frog_square.26d5a2eb.jpeg"
+  },
+  {
+    "revision": "709c61f913a44bf72565802f2391c818",
+    "url": "/static/media/hand2_square.709c61f9.jpeg"
+  },
+  {
+    "revision": "3787a235921a1a78b29d2f563e35c7de",
+    "url": "/static/media/jellyfish_square.3787a235.jpeg"
+  },
+  {
+    "revision": "6a1a8e2f3ab6dfdcf7552dc66b9d2135",
+    "url": "/static/media/mask_square.6a1a8e2f.jpg"
+  },
+  {
+    "revision": "20e6663c2f4124dcd9fe68379369cdb7",
+    "url": "/static/media/mushroom_square.20e6663c.jpg"
+  },
+  {
+    "revision": "b017a80e1c387254349a40cb5d50a390",
+    "url": "/static/media/mushrooms_square.b017a80e.jpg"
+  },
+  {
+    "revision": "828f5e3f3a1e34d6b6f52dc1e164d853",
+    "url": "/static/media/orange_square.828f5e3f.jpg"
+  },
+  {
+    "revision": "7c7b61ed9cfff52e6d32e0af86c2d0ab",
+    "url": "/static/media/owl_square.7c7b61ed.jpg"
+  },
+  {
+    "revision": "0f3b6b8ac454df1d008b9d62677a8e17",
+    "url": "/static/media/peacock_square.0f3b6b8a.jpg"
+  },
+  {
+    "revision": "e43d2b0f4d771a5f240f7d29f2e9a347",
+    "url": "/static/media/skull_square.e43d2b0f.jpg"
+  }
+]);
