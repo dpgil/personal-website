f2caab6c635d11289234f3a12efbc49d6958ae59
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-28 22:09:58 -0400

    More refactoring

---
+++ src/pages/music/MusicBlock.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { MusicPlayer } from "./MusicPlayer";
+import { Song } from "../../noise";
+
+const wrapperStyle: React.CSSProperties = {
+  width: "90%",
+  maxWidth: "700px",
+  margin: "auto",
+  paddingBottom: 25
+};
+
+const titleStyle: React.CSSProperties = {
+  textAlign: "left"
+};
+
+const playerStyle: React.CSSProperties = {
+  outline: "none",
+  width: "100%"
+};
+
+interface MusicBlockProps {
+  song: Song;
+}
+
+export const MusicBlock: React.FC<MusicBlockProps> = props => {
+  const { song } = props;
+
+  return (
+    <div style={wrapperStyle}>
+      <div style={titleStyle}>{song.name}</div>
+      <MusicPlayer style={playerStyle} src={song.src} />
+    </div>
+  );
+};
--- src/pages/music/MusicPage.tsx
+++ src/pages/music/MusicPage.tsx
@@ -1,32 +1,13 @@
 import React from "react";
-import { MusicPlayer } from "./MusicPlayer";
 import { songs } from "../../noise";
-
-const playerWrapperStyle: React.CSSProperties = {
-  width: "90%",
-  maxWidth: "700px",
-  margin: "auto",
-  paddingBottom: 25
-};
-
-const titleStyle: React.CSSProperties = {
-  textAlign: "left"
-};
-
-const playerStyle: React.CSSProperties = {
-  outline: "none",
-  width: "100%"
-};
+import { MusicBlock } from "./MusicBlock";
 
 export const MusicPage = () => {
   return (
     <div>
       <div>music page!</div>
       {songs.map(s => (
-        <div key={s.name} style={playerWrapperStyle}>
-          <div style={titleStyle}>{s.name}</div>
-          <MusicPlayer style={playerStyle} src={s.src} />
-        </div>
+        <MusicBlock key={s.name} song={s} />
       ))}
     </div>
   );
