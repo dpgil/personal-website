98fd7feb338a4f4fd163af648f760940445ac873
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-28 21:43:57 -0400

    More styling

--- src/pages/music/MusicPage.tsx
+++ src/pages/music/MusicPage.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import { MusicPlayer } from "./MusicPlayer";
 import { songs } from "../../noise";
 
-const playerWrapperStyle: React.CSSProperties = {};
+const playerWrapperStyle: React.CSSProperties = {
+  width: "90%",
+  maxWidth: "700px",
+  margin: "auto",
+  paddingBottom: 25
+};
+
+const titleStyle: React.CSSProperties = {
+  textAlign: "left"
+};
 
 const playerStyle: React.CSSProperties = {
-  outline: "none"
+  outline: "none",
+  width: "100%"
 };
 
 export const MusicPage = () => {
@@ -14,6 +24,7 @@ export const MusicPage = () => {
       <div>music page!</div>
       {songs.map(s => (
         <div key={s.name} style={playerWrapperStyle}>
+          <div style={titleStyle}>{s.name}</div>
           <MusicPlayer style={playerStyle} src={s.src} />
         </div>
       ))}
