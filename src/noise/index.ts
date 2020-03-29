dbca0331347497e6f3cd3063929a1642b6912c07
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-29 00:26:33 -0400

    Add maybe let

--- src/noise/index.ts
+++ src/noise/index.ts
@@ -1,9 +1,9 @@
 const comfortable = require("./comfortable.mp3");
+const maybe_let = require("./maybe_let.wav");
 
 export type Song = { name: string; src: string };
 
 export const songs: Song[] = [
   { name: "comfortable", src: comfortable },
-  { name: "comfortable-1", src: comfortable },
-  { name: "comfortable-2", src: comfortable }
+  { name: "maybe let", src: maybe_let }
 ];
