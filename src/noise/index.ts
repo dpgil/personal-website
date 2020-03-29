1c0ff752ebdfad26fb1981a8b1a0d294466d20ee
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-29 00:46:09 -0400

    Add shine

--- src/noise/index.ts
+++ src/noise/index.ts
@@ -3,13 +3,15 @@ const comfortable = require("./comfortable.mp3");
 const flying = require("./flying.wav");
 const glass = require("./glass.wav");
 const maybe_let = require("./maybe_let.wav");
+const shine = require("./shine.wav");
 
 export type Song = { name: string; src: string };
 
 export const songs: Song[] = [
   { name: "comfortable", src: comfortable },
-  { name: "maybe let", src: maybe_let },
-  { name: "glass", src: glass },
   { name: "flying ft. chet", src: flying },
+  { name: "maybe let", src: maybe_let },
+  { name: "shine", src: shine },
+  { name: "glass", src: glass },
   { name: "chimes (demo)", src: chimes }
 ];
