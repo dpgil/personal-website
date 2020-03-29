21d8a7c8f6aa69e0d091cebff45a59b882823695
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-29 00:34:24 -0400

    Add glass

--- src/noise/index.ts
+++ src/noise/index.ts
@@ -1,9 +1,11 @@
 const comfortable = require("./comfortable.mp3");
 const maybe_let = require("./maybe_let.wav");
+const glass = require("./glass");
 
 export type Song = { name: string; src: string };
 
 export const songs: Song[] = [
   { name: "comfortable", src: comfortable },
-  { name: "maybe let", src: maybe_let }
+  { name: "maybe let", src: maybe_let },
+  { name: "glass", src: glass }
 ];
