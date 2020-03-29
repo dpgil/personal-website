193aa64bb22273c9807430e0faca3b616b056268
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-29 00:38:25 -0400

    Add chimes

--- src/noise/index.ts
+++ src/noise/index.ts
@@ -1,11 +1,13 @@
+const chimes = require("./chimes.wav");
 const comfortable = require("./comfortable.mp3");
+const glass = require("./glass.wav");
 const maybe_let = require("./maybe_let.wav");
-const glass = require("./glass");
 
 export type Song = { name: string; src: string };
 
 export const songs: Song[] = [
   { name: "comfortable", src: comfortable },
   { name: "maybe let", src: maybe_let },
-  { name: "glass", src: glass }
+  { name: "glass", src: glass },
+  { name: "chimes (demo)", src: chimes }
 ];
