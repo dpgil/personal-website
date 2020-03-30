aa38007540d3aebb07fd78730ac39f969a8b8afc
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-30 14:50:08 -0400

    Make oyster letters

--- src/pages/landing/Board.tsx
+++ src/pages/landing/Board.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { PuzzleState } from "./LandingPage";
 
 interface BoardProps {
   size: number;
+  setPuzzleState: (puzzleState: PuzzleState) => void;
 }
 
 export const Board: React.FC<BoardProps> = props => {
@@ -35,13 +37,16 @@ const TileRow: React.FC<RowProps> = props => {
   return <div style={rowStyle}>{tiles}</div>;
 };
 
-const getTileStyle = (rotate: number): React.CSSProperties => {
+const getTileStyle = (
+  rotate: number,
+  isTilted: boolean
+): React.CSSProperties => {
   return {
     display: "inline-block",
     margin: tileMargin,
     width: 50,
     height: 50,
-    backgroundColor: "#e0e0e0",
+    backgroundColor: isTilted ? "#f9b49b" : "#e0e0e0",
     borderRadius: 5,
     transform: `rotate(${rotate}deg)`,
     transition: "all 0.4s ease-in-out"
@@ -50,10 +55,12 @@ const getTileStyle = (rotate: number): React.CSSProperties => {
 
 const Tile: React.FC = () => {
   const [rotate, setRotate] = React.useState<number>(0);
+  const [isTilted, setTilted] = React.useState<boolean>(false);
 
   const onClick = () => {
     setRotate(r => r + 45);
+    setTilted(t => !t);
   };
 
-  return <div onClick={onClick} style={getTileStyle(rotate)}></div>;
+  return <div onClick={onClick} style={getTileStyle(rotate, isTilted)}></div>;
 };
--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -10,11 +10,22 @@ const pageStyle: React.CSSProperties = {
   paddingBottom: 20
 };
 
+export type PuzzleState = boolean[];
+
 export const LandingPage: React.FC = () => {
+  const [puzzleState, setPuzzleState] = React.useState<PuzzleState>([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ]);
+
   return (
     <div style={pageStyle}>
-      <Description />
-      <Board size={boardSize} />
+      <Description puzzleState={puzzleState} />
+      <Board size={boardSize} setPuzzleState={setPuzzleState} />
     </div>
   );
 };
@@ -24,11 +35,48 @@ const descriptionStyle: React.CSSProperties = {
   paddingBottom: 10
 };
 
-const Description: React.FC = () => {
+const Description: React.FC<{ puzzleState: PuzzleState }> = props => {
   return (
     <div style={descriptionStyle}>
       hi, i'm daniel gil (not the american ninja warrior). check out my pages or
-      try to solve this puzzle, or both, or neither. the world is your oyster
+      try to solve this puzzle, or both, or neither. the world is your{" "}
+      <Oyster puzzleState={props.puzzleState} />
     </div>
+  );
+};
+
+const getLetterStyle = (solved: boolean): React.CSSProperties => {
+  return {
+    color: solved ? "#f9b49b" : "inherit"
+  };
+};
+
+const Oyster: React.FC<{ puzzleState: PuzzleState }> = props => {
+  const { puzzleState } = props;
+  return (
+    <>
+      <OysterLetter c="o" i={0} puzzleState={puzzleState} />
+      <OysterLetter c="y" i={1} puzzleState={puzzleState} />
+      <OysterLetter c="s" i={2} puzzleState={puzzleState} />
+      <OysterLetter c="t" i={3} puzzleState={puzzleState} />
+      <OysterLetter c="e" i={4} puzzleState={puzzleState} />
+      <OysterLetter c="r" i={5} puzzleState={puzzleState} />
+    </>
+  );
+};
+
+interface OysterLetterProps {
+  c: string;
+  i: number;
+  puzzleState: PuzzleState;
+}
+
+const OysterLetter: React.FC<OysterLetterProps> = props => {
+  const { c, i, puzzleState } = props;
+
+  return (
+    <span style={getLetterStyle(puzzleState.length > i && puzzleState[i])}>
+      {c}
+    </span>
   );
 };
