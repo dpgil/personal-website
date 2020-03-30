34a78d6920345a310ca8bafd4e79a9a223ae48f8
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-30 12:18:26 -0400

    Add description

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -2,11 +2,32 @@ import React from "react";
 
 const boardSize = 5;
 
+const pageStyle: React.CSSProperties = {
+  width: "90%",
+  maxWidth: "700px",
+  margin: "auto",
+  paddingBottom: 20
+};
+
 export const LandingPage: React.FC = () => {
   return (
-    <div>
-      <div>landing page</div>
+    <div style={pageStyle}>
+      <Description />
       <GameBoard size={boardSize} />
+    </div>
+  );
+};
+
+const descriptionStyle: React.CSSProperties = {
+  textAlign: "left",
+  paddingBottom: 10
+};
+
+const Description: React.FC = () => {
+  return (
+    <div style={descriptionStyle}>
+      hi, i'm daniel gil (not the american ninja warrior). check out my pages or
+      try to solve this puzzle, or both, or neither. the world is your oyster
     </div>
   );
 };
