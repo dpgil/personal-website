76a4bef0a07531cee78528717cae088fa5a6a1ce
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-03-30 17:12:47 -0400

    Refactor

--- src/pages/landing/Board.tsx
+++ src/pages/landing/Board.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { generateKey, oysterStateFromBoardState } from "./solution";
 import { OysterState } from "./Oyster";
+import { TileRow } from "./TileRow";
 
 interface BoardProps {
   size: number;
@@ -51,65 +52,4 @@ export const Board: React.FC<BoardProps> = props => {
   }
 
   return <div style={boardStyle}>{elements}</div>;
-};
-
-const tileMargin = 10;
-
-const rowStyle: React.CSSProperties = {
-  marginLeft: -tileMargin,
-  marginRight: -tileMargin
-};
-
-interface RowProps {
-  size: number;
-  rowIndex: number;
-  tilted: boolean[];
-  toggleTilted: (cellIndex: number) => () => void;
-}
-
-const TileRow: React.FC<RowProps> = props => {
-  const { size, rowIndex, tilted, toggleTilted } = props;
-
-  const tiles: JSX.Element[] = [];
-
-  for (let i = 0; i < size; i++) {
-    const cellIndex = rowIndex * size + i;
-    tiles.push(
-      <Tile
-        key={`c${cellIndex}`}
-        tilted={tilted.length > cellIndex && tilted[cellIndex]}
-        toggleTilted={toggleTilted(cellIndex)}
-      />
-    );
-  }
-
-  return <div style={rowStyle}>{tiles}</div>;
-};
-
-const getTileStyle = (isTilted: boolean): React.CSSProperties => {
-  return {
-    display: "inline-block",
-    margin: tileMargin,
-    width: 50,
-    height: 50,
-    backgroundColor: isTilted ? "#f9b49b" : "#e0e0e0",
-    borderRadius: 5,
-    transform: `rotate(${isTilted ? 45 : 0}deg)`,
-    transition: "all 0.4s ease-in-out"
-  };
-};
-
-interface TileProps {
-  tilted: boolean;
-  toggleTilted: () => void;
-}
-
-const Tile: React.FC<TileProps> = props => {
-  const { tilted, toggleTilted } = props;
-
-  const onClick = () => {
-    toggleTilted();
-  };
-
-  return <div onClick={onClick} style={getTileStyle(tilted)}></div>;
 };
