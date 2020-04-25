16c1fd3f2b97153465fea4cb859b78b51add5219
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-04-25 17:37:15 -0400

    Initial detail

--- src/pages/art/ArtPage.tsx
+++ src/pages/art/ArtPage.tsx
@@ -2,15 +2,34 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { ImageBlock } from "./ImageBlock";
 import { images } from "../../media/art";
+import { Link } from "react-router-dom";
+import { Routes } from "../../routes";
 
 export const ArtPage: React.FC = () => {
+  const hash = window.location.hash;
+  const mainPage = `#/${Routes.ArtPage.path}`;
+  if (hash.length > mainPage.length) {
+    const path = hash.substr(mainPage.length);
+    const details = images.find(i => i.path === path);
+    if (details !== undefined) {
+      // We're in the detail page.
+      return <DetailBlock />;
+    }
+  }
+
   return (
     <div>
       <Grid>
         {images.map(i => (
-          <ImageBlock key={i.name} image={i} />
+          <Link to={`${Routes.ArtPage.path}/${i.path}`} key={i.name}>
+            <ImageBlock image={i} />
+          </Link>
         ))}
       </Grid>
     </div>
   );
+};
+
+const DetailBlock: React.FC = () => {
+  return <div>hallo</div>;
 };
