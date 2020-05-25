98916d03bd2530070217ad81b529ac4dd2753e62
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-05-25 13:02:04 -0400

    fix

--- src/media/blogs/index.ts
+++ src/media/blogs/index.ts
@@ -2,7 +2,7 @@ import arreglos from "./2020-03-31-arreglos";
 import busqueda from "./2020-04-26-busqueda";
 import hack from "./2020-05-01-hack";
 import gambling from "./2020-05-02-gambling";
-import observability from "./2020-05-16-observability";
+import observability from "./2020-05-25-observability";
 import { Language } from "./language";
 
 export type Blog = {
